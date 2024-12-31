1e984485c160e79faf3d8bd920bb8342cab55160
samikshagaiki <samikshagaiki@gmail.com>
2024-12-31 17:30:28 +0530

    updated

--- polaris/src/Pages/Quiz.jsx
+++ polaris/src/Pages/Quiz.jsx
@@ -50,64 +50,64 @@ const Quiz = () => {
 
   return (
     <div className="h-screen w-screen bg-gradient-to-b from-blue-800 to-black flex flex-col items-center justify-center relative overflow-hidden">
-      <SparkleBackground/>
+      <SparkleBackground />
 
-    <div className="quiz-container">
-      <div class="quiz-content relative  text-center p-5 max-w-lg mx-auto">
-      {!isSubmitted ? (
-        <>
-        <Timer/>
-          <div className="question-container  bg-blue-900 items-center mb-5 justify-center border-2 border-yellow-400 rounded-lg">
-                <h1 className="mt-6 text-3xl text-yellow-400">Question {currentQues + 1}</h1>
-                <p className="mb-6 text-2xl text-white">{ques[currentQues].ques}</p>
-            </div>
-          <ul class="list-none p-0 grid grid-cols-2 gap-4 justify-center items-center">
-            {ques[currentQues].options.map((option) => (
-              <li
+      <div className="quiz-container">
+        <div class="quiz-content relative  text-center p-5 max-w-lg mx-auto">
+          {!isSubmitted ? (
+            <>
+              <Timer />
+              <div className="question-container bg-blue-900 items-center mb-5 justify-center border-2 border-yellow-400 rounded-lg cursor-none">
+                <h1 className="mt-6 text-3xl text-yellow-400">
+                  Question {currentQues + 1}
+                </h1>
+                <p className="mb-6 text-2xl text-white">
+                  {ques[currentQues].ques}
+                </p>
+              </div>
 
-                key={option}
-                className={`flex items-center justify-center p-4 border-2 rounded-lg cursor-pointer transition-transform duration-200 transform hover:scale-105 shadow-md w-56 ${
-                  selectedOption === option
-                    ? option === ques[currentQues].correctAnswer
-                      ? highlight === "green"
-                        ? "bg-green-500 border-green-700"
-                        : ""
-                      : highlight === "red"
-                      ? "bg-red-500 border-red-700"
-                      : ""
-                    : "bg-blue-900 border-yellow-400"
-                }`
-                
-              }
+              <ul className="list-none p-0 grid grid-cols-2 gap-4 justify-center items-center cursor-none">
+                {ques[currentQues].options.map((option) => (
+                  <li
+                    key={option}
+                    className={`flex items-center justify-center p-4 border-2 rounded-lg cursor-pointer transition-transform duration-200 transform hover:scale-105 shadow-md w-56 ${
+                      selectedOption === option
+                        ? option === ques[currentQues].correctAnswer
+                          ? highlight === "green"
+                            ? "bg-green-500 border-green-700"
+                            : ""
+                          : highlight === "red"
+                          ? "bg-red-500 border-red-700"
+                          : ""
+                        : "bg-blue-900 border-yellow-400"
+                    }`}
+                  >
+                    <button
+                      type="button"
+                      className="w-full transform p-2 text-center rounded-lg transition duration-300 focus:outline-none active:bg-blue-700 hover:bg-blue-800"
+                      onClick={() => handleOptionSelect(option)}
+                    >
+                      {option}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+
+              <button
+                className="bg-yellow-400 text-black text-lg px-5 py-2.5 rounded mt-5 transition duration-300 hover:enabled:bg-yellow-500 hover:enabled:shadow-lg disabled:bg-gray-600 disabled:cursor-not-allowed"
+                onClick={handleSubmit}
+                disabled={!selectedOption}
               >
-                <label className="w-full cursor-pointer">
-                  <input
-                    type="radio"
-                    className="transform scale-150 mr-2"
-                    name="option"
-                    value={option}
-                    checked={selectedOption === option}
-                    onChange={() => handleOptionSelect(option)}
-                  />
-                  {option}
-                </label>
-              </li>
-            ))}
-          </ul>
-          <button 
-            className={`bg-yellow-400 text-black text-lg px-5 py-2.5 rounded mt-5 transition duration-300 hover:enabled:bg-yellow-500 hover:enabled:shadow-lg disabled:bg-gray-600 disabled:cursor-not-allowed`}
-            onClick={handleSubmit}
-            disabled={!selectedOption}>
-                 Submit
-          </button>
-        </>
-      ) : (
-        <div>
-          <h1 class="mt-5 text-3xl text-yellow-400">Quiz Completed!</h1>
+                Submit
+              </button>
+            </>
+          ) : (
+            <div>
+              <h1 class="mt-5 text-3xl text-yellow-400">Quiz Completed!</h1>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-    </div>
+      </div>
     </div>
   );
 };
